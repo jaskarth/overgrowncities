c9466a4f833d6e20a45a805071435679338176c1
SuperCoder7979 <k.pranav@gmail.com>
2020-03-19 16:15:52 -0400

    init repo

---
+++ src/main/java/supercoder79/overgrowncities/Overgrowncities.java
@@ -0,0 +1,10 @@
+package supercoder79.overgrowncities;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Overgrowncities implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/supercoder79/overgrowncities/OvergrowncitiesClient.java
@@ -0,0 +1,13 @@
+package supercoder79.overgrowncities;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.Environment;
+import net.fabricmc.api.EnvType;
+
+@Environment(EnvType.CLIENT)
+public class OvergrowncitiesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
